4bfd8c712336e32b7dcd1fdf6d0c09bb1c8f086e
thiago <thiagoprocaci@gmail.com>
2019-02-27 18:54:28 -0300

    utf-8

--- src/main/java/com/tbp/Application.java
+++ src/main/java/com/tbp/Application.java
@@ -2,6 +2,9 @@ package com.tbp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 
 @SpringBootApplication
